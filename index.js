7bb7c2ef93948d10928f674620cc2de876c36cd6
hanlei <hanlei@sogou-inc.com>
2019-05-30 12:11:16 +0800

    fix garbled

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const { VM } = require('vm2');
-const request = require('./lib/request');
+const { VM } = require('vm2')
+const fetch = require('node-fetch')
 
 module.exports = async function jsonpClient(url) {
-  const resBody = await request(url);
-  const callbackNameReg = /^([$\w_][\d\w_$]+)\(/;
-  const jsonpStartReg = /^\(/;
-  const jsonpEndReg = /\);?$/m;
-  const isJsonpStart = jsonpStartReg.test(resBody);
-  const isJsonpEnd = jsonpEndReg.test(resBody);
-  let jsonp = resBody;
-  let callbackName = '';
+  const resBody = await fetch(url).then(res => res.text())
+  const callbackNameReg = /^([$\w_][\d\w_$]+)\(/
+  const jsonpStartReg = /^\(/
+  const jsonpEndReg = /\);?$/m
+  const isJsonpStart = jsonpStartReg.test(resBody)
+  const isJsonpEnd = jsonpEndReg.test(resBody)
+  let jsonp = resBody
+  let callbackName = ''
   if (callbackNameReg.test(resBody) && isJsonpEnd) {
-    callbackName = jsonp.match(callbackNameReg)[1];
+    callbackName = jsonp.match(callbackNameReg)[1]
   } else if (isJsonpStart && isJsonpEnd) {
-    callbackName = 'jsonp_' + Date.now().toString();
-    jsonp = callbackName + jsonp;
+    callbackName = 'jsonp_' + Date.now().toString()
+    jsonp = callbackName + jsonp
   } else {
-    return resBody;
+    return resBody
   }
   const vm = new VM({
     timeout: 1000,
     sandbox: {
       [callbackName](data) {
         try {
-          return JSON.stringify(data);
+          return JSON.stringify(data)
         } catch (error) {
-          return data.toString();
+          return data.toString()
         }
       }
     }
-  });
-  return vm.run(jsonp);
-};
+  })
+  return vm.run(jsonp)
+}
